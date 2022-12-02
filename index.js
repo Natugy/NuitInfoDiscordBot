8d8f05b096a316cca544acc17bc40b1e9cf32513
Natugy <tanguy.sergeant@gmail.com>
2022-12-02 02:11:27 +0100

    add chat command

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { Client, GatewayIntentBits,SlashCommandBuilder } = require('discord.js');
+const { Client, GatewayIntentBits,SlashCommandBuilder, TimestampStyles } = require('discord.js');
 const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
-const { doc, getDoc } = require("firebase/firestore"); 
+const { doc, getDoc, addDoc, collection, Timestamp } = require("firebase/firestore"); 
 require("dotenv").config();
 
 const firebaseConfig = {
@@ -11,7 +11,7 @@ const firebaseConfig = {
     storageBucket: "ndi-game-d9b14.appspot.com",
     messagingSenderId: "34549845840",
     appId: "1:34549845840:web:d019c3556207d0ee926655"
-  };
+};
   
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -33,12 +33,13 @@ const client = new Client({
 
 client.on('ready', async () => {
     client.application.commands.create(data);
-    client.guilds.cache.get("1046876578803630131").commands.create(data);
+    client.application.commands.create(addMessage);
+   // client.guilds.cache.get("1046876578803630131").commands.create(data);
     console.log(`Logged succesfully!`);
-    const docRef = doc(db,"test", "client");
+  /*  const docRef = doc(db,"test", "client");
     const docSnap = await getDoc(docRef);
     const dataDoc = docSnap.data();
-    console.log(dataDoc);
+    console.log(dataDoc);*/
 });
 
 const data = new SlashCommandBuilder()
@@ -51,7 +52,18 @@ const data = new SlashCommandBuilder()
          .setRequired(false)
          );
 
-client.on("interactionCreate", interraction => {
+const addMessage = new SlashCommandBuilder()
+        .setName("chat")
+        .setDescription("Ajoute un message au chat")
+        .addStringOption(option =>
+            option
+            .setName("message")
+            .setDescription("Message a ajouté")
+            .setRequired(true));
+
+
+
+client.on("interactionCreate", async interraction =>  {
     if(interraction.isCommand()){
         if(interraction.commandName === "baste"){
             let user =interraction.options.getUser("utilisateur");
@@ -59,6 +71,25 @@ client.on("interactionCreate", interraction => {
                 interraction.reply("pong <@"+user.id+">");
             }
             interraction.reply("tu pense pouvoir invoquer notre dieu ? tu n'as pas ce genre de pouvoir");
+        }
+
+        if(interraction.commandName ==="chat"){
+            let author = interraction.user.tag
+            let message = interraction.options.getString("message");
+            
+            try {
+                const docRef = await addDoc(collection(db, "chat"), {
+                userTag: author,
+                content: message,
+                date : Timestamp.now()
+                });
+            
+                interraction.reply("Message envoyé : " + message);
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
+            
+            console.log(author);
         }
     }
 });
